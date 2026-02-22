00e01b3a23707f2a499e1893b2408c45bf87f74b
Jeff Cameron <cameron.jp@gmail.com>
2026-02-22 20:13:36 +0000

    Webserver that stores short json messages.

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const app = express()
+const port = 80
+
+const messageQueue = [];
+const maxQueueLength = 1000;
+
+function ThrowOutOldMessages() {
+    while (messageQueue.length > maxQueueLength) {
+	messageQueue.shift();
+    }
+}
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.post('/putmessage', (req, res) => {
+    messageQueue.push(req.body);
+    ThrowOutOldMessages();
+    res.json({
+	message: 'success',
+    });
+});
+
+app.get('/recentmessages', (req, res) => {
+    ThrowOutOldMessages();
+    res.json({
+	recentMessages: messageQueue,
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
